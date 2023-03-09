c28e46d8582c40ba9ff2f7ef971c6af24b9b956b
connerlinskens <conner@linskens.nl>
2023-03-09 14:42:18 +0100

    Added PugiXML library

---
+++ Services/XMLService.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Conner on 3/9/2023.
+//
+
+#include "XMLService.h"
+#include "pugixml.hpp"
+
+std::string XMLService::LoadFile(const std::string &path) {
+    return {};
+}
+
+void XMLService::SaveFile(const std::string &path, const std::string &content) {
+}
